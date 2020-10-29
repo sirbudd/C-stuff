f7d1365212ff1f29470601163c5c93d65b9bb811
sirbudd <dragos.daniel199@gmail.com>
2020-10-29 14:09:25 +0200

    test

---
+++ basic2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Write a C program to print your name, date of birth. and mobile number.
+
+int main()
+{
+    printf("Sirbu Dragos\n");
+    printf("DOB : 03.01.1999\n");
+    printf("Mobile : 0720 000 000\n");
+    
+    return 0;
+}
