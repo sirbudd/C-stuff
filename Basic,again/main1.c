c5d250c4f5eda1ad93b80fd3eba1420fffe22792
Sirbu Dragos <dragos.daniel199@gmail.com>
2021-01-24 17:43:27 +0200

    working

---
+++ Basic,again/main1.c
@@ -0,0 +1,26 @@
+//overflow
+//WARNINGS
+//CASTING
+#include <stdio.h>
+
+int main()
+{
+    unsigned char x = 255;
+    x = x + 10;
+    printf("%x\n",x); //pt ca e unsigned overflowul este ca un reset
+
+    char y = 127;
+    y = y + 2;
+    printf("%u\n",y); //pt ca e signed overflowul e random
+
+    char z = 1000; //warning de overflow
+
+    int a = 5;
+    float b = 10.2;
+
+    double sum = (double)a+b;
+
+    printf("%lf\n",sum);
+
+    return 0;
+}
