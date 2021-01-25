191e41eb252982ee9c6cff7584d2651d7006f561
Sirbu Dragos <dragos.daniel199@gmail.com>
2021-01-25 18:02:11 +0200

    ok

--- Basic,again/arrays2.c
+++ Basic,again/arrays2.c
@@ -6,6 +6,29 @@ int main()
 {
     int x[5][5];
     int i,j;
+    int n =0,m=0;
+
+    printf("N = ");
+    scanf("%d",&n);
+    printf("M = ");
+    scanf("%d",&m);
+
+    for (i = 0; i < n ;i++)
+        {
+            for (j = 0;j < m; j++)
+            {
+                printf("Valori = ");
+                scanf("%d",&x[i][j]);
+            }
+        }
+
+    for (i = 0; i < n ;i++)
+        {
+            for (j = 0;j < m; j++)
+            {
+                printf("Rezultatul [%d][%d] = %d\n",i+1,j+1,x[i][j]);
+            }
+        }
 
 
     return 0;
