64a15c9196d7a18ca38e6d82292a16938daa1c99
Sirbu Dragos <dragos.daniel199@gmail.com>
2021-04-20 15:22:50 +0300

    working

--- Recap-2/main1.c
+++ Recap-2/main1.c
@@ -8,10 +8,18 @@ int main()
     int x = 10;
     printf("This is my number , X = %d\n",x);
     char* string = "Testing,testing";
+    char* strin2 = "My name is what";
     printf(string);
     printf("\n");
+    printf("%s\n",strin2);
 
+    unsigned int y = -20;
+    printf("Y (unsigned) = %u\n",y);
 
+    int overflow = 2000000000000000000000000;
+    printf("Int cu overflow = %d\n",overflow);
+
+    double nr_lung = 2323232.421;
 
     return 0;
 }
