3bfd3e2a73b0ff177496141071e0574bf37bd97a
Sirbu Dragos <dragos.daniel199@gmail.com>
2021-04-20 14:48:47 +0300

    started recap

---
+++ Recap-2/main1.c
@@ -0,0 +1,17 @@
+//basic data types
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 10;
+    printf("This is my number , X = %d\n",x);
+    char* string = "Testing,testing";
+    printf(string);
+    printf("\n");
+
+
+
+    return 0;
+}
